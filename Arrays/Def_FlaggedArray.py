087df3cc474304d933fe4a3da672333ee62778b4
AmirBl82 <98527218+AmirBl82@users.noreply.github.com>
2024-10-13 11:27:29 +0330

    Ds_Python Codes

--- Arrays/Def_FlaggedArray.py
+++ Arrays/Def_FlaggedArray.py
@@ -1,6 +1,6 @@
 class FlaggedArray:
     def __init__(self, capacity):
-        self.A = [None] * capacity  
+        self.array = [None] * capacity  
         self.flag = [False] * capacity  
         self.capacity = capacity  
         self.size = 0  
@@ -15,21 +15,21 @@ class FlaggedArray:
         if 0 <= index < self.capacity:
             if not self.flag[index]:  
                 self.size += 1  
-            self.A[index] = value
+            self.array[index] = value
             self.flag[index] = True  
         else:
             print("Index is out of bounds")
 
     def remove(self, index):
         if 0 <= index < self.capacity and self.flag[index]: 
-            self.A[index] = None
+            self.array[index] = None
             self.flag[index] = False  
             self.size -= 1  
         else:
             print("Index is either out of bounds or array is empty")  
 
     def print(self):
-        print("Array:", self.A)
+        print("Array:", self.array)
         print("Flags:", self.flag)
 
 arr = FlaggedArray(10)
