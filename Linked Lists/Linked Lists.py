3c9004b440b5c2919368b96679c56b8321186346
AmirBl82 <amirshayanbolandzadeh@gmail.com>
2024-11-27 17:32:11 +0330

    DS_Python Codes

--- Linked Lists/Linked Lists.py
+++ Linked Lists/Linked Lists.py
@@ -4,13 +4,13 @@ class Node:
         self.next = None
 
 class LinkedList:
-    def __init__(self,dataType = int):
-        self.head = None
+    def __init__(self, dataType=int):
+        self._head = None  # Private head
         self.length = 0
         self.dataType = dataType
 
     def __str__(self):
-        temp_node = self.head
+        temp_node = self._head
         result = ""
         while temp_node:
             result += str(temp_node.value)
@@ -20,7 +20,6 @@ class LinkedList:
         return result
 
     # Insert an element at a specific index
-    # Time Complexity is O(n)
     def insert(self, index, value):
         if not isinstance(value, self.dataType):
             raise TypeError(f"Linked List only accepts elements of type {self.dataType}") 
@@ -29,10 +28,10 @@ class LinkedList:
             raise IndexError("Index out of Bounds")
 
         if index == 0:  # Insert at the beginning
-            new_node.next = self.head
-            self.head = new_node
+            new_node.next = self._head
+            self._head = new_node
         else:
-            temp_node = self.head
+            temp_node = self._head
             for _ in range(index - 1):
                 temp_node = temp_node.next
             new_node.next = temp_node.next
@@ -42,14 +41,13 @@ class LinkedList:
         return True
 
     # Remove an element at a specific index
-    # Time Complexity is O(n)
     def remove(self, index):
         if index < 0 or index >= self.length:
             raise IndexError("Index out of Range") 
 
         if index == 0:  # Remove the first element
-            removed_node = self.head
-            self.head = self.head.next
+            removed_node = self._head
+            self._head = self._head.next
         else:
             prev_node = self.get(index - 1)
             removed_node = prev_node.next
@@ -60,25 +58,24 @@ class LinkedList:
         return removed_node
 
     # Traverse the linked list
-    # Time Complexity is O(n)
     def traverse(self):
-        current_node = self.head
+        current_node = self._head
         while current_node:
             print(current_node.value)
             current_node = current_node.next
 
     # Get the node at a specific index
-    # Time Complexity is O(n)
     def get_value(self, index):
         if index < 0 or index >= self.length:
             raise IndexError("Index out of Range") 
-        current_node = self.head
+        current_node = self._head
         for _ in range(index):
             current_node = current_node.next
         return current_node
     
+    # Get node address by value
     def get_address(self, value):
-        current_node = self.head
+        current_node = self._head
         while current_node:
             if current_node.value == value:
                 return current_node  # Return the node itself (reference to the node)
@@ -86,7 +83,6 @@ class LinkedList:
         return "The Element does not exist"
 
     # Update the value of a node at a specific index
-    # Time Complexity is O(n)
     def set_value(self, index, value):
         temp_node = self.get(index)
         if temp_node:
@@ -95,10 +91,15 @@ class LinkedList:
         return False
 
     # Delete all nodes in the linked list
-    # Time Complexity is O(1)
     def delete_all(self):
-        self.head = None
+        self._head = None
         self.length = 0
+
+    # Get the head
+    def get_first(self):
+        if self._head:
+            return self._head
+        return "Linked List is Empty"
 
 linked_list = LinkedList()
 linked_list.insert(0, 10)
@@ -106,7 +107,12 @@ linked_list.insert(1, 20)
 
 node = linked_list.get_address(10)
 node1 = linked_list.get_address(20)
-index = linked_list.get_value(1)
+index = linked_list.get_value(0)
+first_node = linked_list.get_first()
+
 print(id(node))
 print(id(node1))
 print(index.value)
+print(first_node.value)
+
+
