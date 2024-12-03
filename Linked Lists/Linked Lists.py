eee67c81dc1a430d3546f62d38c6d77b1b9e391e
AmirBl82 <amirshayanbolandzadeh@gmail.com>
2024-12-04 00:07:43 +0330

    DS_Python Codes

--- Linked Lists/Linked Lists.py
+++ Linked Lists/Linked Lists.py
@@ -1,12 +1,12 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
 
 class LinkedList:
-    def __init__(self,dataType = int):
+    def __init__(self, dataType=int):
         self.__head = None
-        self.length = 0
+        self.__length = 0
         self.dataType = dataType
     
     def __str__(self):
@@ -19,11 +19,14 @@ class LinkedList:
             temp_node = temp_node.next
         return result
     
-    def insert(self,index,value):
+    def get_length(self):
+        return self.__length
+    
+    def insert(self, index, value):
         if not isinstance(value, self.dataType):
             raise TypeError(f"Linked List only accepts elements of type {self.dataType}")
         
-        if index < 0 or index > self.length:
+        if index < 0 or index > self.__length:
             raise IndexError("Index out of Bounds")
         
         new_node = Node(value)
@@ -32,25 +35,25 @@ class LinkedList:
             self.__head = new_node
         else:
             temp_node = self.__head
-            for _ in range(index-1):
-                temp_node.next = temp_node
+            for _ in range(index - 1):
+                temp_node = temp_node.next
             new_node.next = temp_node.next
             temp_node.next = new_node
         
-        self.length += 1
+        self.__length += 1
     
-    def append(self,value):
-        self.insert(self.length,value)
+    def append(self, value):
+        self.insert(self.__length, value)
     
-    def prepend(self,value):
-        self.insert(0,value)
+    def prepend(self, value):
+        self.insert(0, value)
     
     def get_first(self):
         if self.__head:
             return self.__head
         return "Linked List is Empty"
     
-    def get_index(self,value):
+    def get_index(self, value):
         current_node = self.__head
         index = 0
         while current_node:
@@ -60,15 +63,15 @@ class LinkedList:
             index += 1
         raise ValueError("Element does not exist")
     
-    def get_value(self,index):
-        if index < 0 or index >= self.length:
+    def get_value(self, index):
+        if index < 0 or index >= self.__length:
             raise IndexError("Index out of bounds")
         current_node = self.__head
         for _ in range(index):
             current_node = current_node.next
         return current_node
     
-    def get_address(self,value):
+    def get_address(self, value):
         current_node = self.__head
         while current_node:
             if current_node.value == value:
@@ -76,7 +79,7 @@ class LinkedList:
             current_node = current_node.next
         raise ValueError("Element does not exist")
     
-    def set_value(self,index,value):
+    def set_value(self, index, value):
         temp_node = self.get_value(index)
         if temp_node:
             temp_node.value = value
@@ -84,23 +87,23 @@ class LinkedList:
         return False
     
     def delete_byIndex(self, index):
-        if index < 0 or index >= self.length:
+        if index < 0 or index >= self.__length:
             raise IndexError("Index out of bounds")
         if index == 0:
             deleted_node = self.__head
             self.__head = self.__head.next
         else:
-            prev_node = self.get_value(index-1)
+            prev_node = self.get_value(index - 1)
             deleted_node = prev_node.next
             prev_node.next = deleted_node.next
         
         deleted_node.next = None
-        self.length -= 1
+        self.__length -= 1
         return deleted_node
     
     def delete_byAddress(self, deleted_node):
         if deleted_node is None:
-            raise ValueError("Value cannot be None")
+            raise ValueError("Value Cannot be None")
         
         self.get_address(deleted_node.value)
         if self.__head == deleted_node:
@@ -114,8 +117,40 @@ class LinkedList:
                 prev_node.next = deleted_node.next
         
         deleted_node.next = None
-        self.length -= 1
+        self.__length -= 1
         return deleted_node
+    
+    def delete_byPrevAddress(self, prev_node):
+        if prev_node is None or prev_node.next is None:
+            raise ValueError("Value Cannot be None")
+
+        deleted_node = prev_node.next
+        prev_node.next = deleted_node.next
+        deleted_node.next = None
+
+        self.__length -= 1
+        return deleted_node
+    
+    def delete_byValue(self, x):
+        if x is None:
+            raise ValueError("x Cannot be None")
+        if self.__head is None:
+            raise ValueError("Linked List is empty")
+
+        if self.__head.value == x:
+            self.__head = self.__head.next
+            self.__length -= 1
+            return
+
+        current_node = self.__head
+        while current_node.next:
+            if current_node.next.value == x:
+                current_node.next = current_node.next.next
+                self.__length -= 1
+                return
+            current_node = current_node.next
+        raise ValueError("Value not found in the Linked List")
+
 
     # Delete all nodes in the linked list
     def delete_all(self):
