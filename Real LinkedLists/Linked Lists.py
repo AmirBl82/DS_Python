66ec8ec670267df0836d56dbfebde203f1d82072
AmirBl82 <98527218+AmirBl82@users.noreply.github.com>
2025-01-17 18:43:45 +0330

    DS_Python Codes

--- Real LinkedLists/Linked Lists.py
+++ Real LinkedLists/Linked Lists.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,value):
         self.value = value
         self.next = None
-        self.prev = None
+
 
 class LinkedList:
     def __init__(self, dataType = int):
